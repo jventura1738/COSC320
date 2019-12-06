212b90f43b39c22c5431b7462cefceae524b8e43
Justin Ventura <justinv9001@aol.com>
2019-12-06 12:45:08 -0500

    draft

---
+++ Project1/matrix.h
@@ -0,0 +1,18 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+class Matrix {
+private:
+	int * matrix;
+
+public:
+	Matrix(int n, int m) : 
+	{
+		matrix = new int[];
+	}
+	~Matrix()
+	{ delete matrix; }
+
+};
+
+#ifndef

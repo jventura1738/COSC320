7ed308a0b37bcbae4828e7bb18edf9acae333cad
Justin <55901784+jventura1738@users.noreply.github.com>
2020-02-02 18:37:57 -0500

    Add files via upload

---
+++ Lab-1/timer.cpp
@@ -0,0 +1,32 @@
+// Justin Ventura COSC220
+// Used for Lab1 in COSC320
+#include "timer.h"
+#include <iomanip>
+
+Timer::Timer() {
+    start_time = 0;
+    end_time = 0;
+}
+
+void Timer::start_timer() {
+    start_time = clock();
+}
+
+void Timer::end_timer() {
+    end_time = clock();
+}
+
+double Timer::get_time() {
+    return 1000*(double(end_time - start_time) / (CLOCKS_PER_SEC));
+}
+
+void Timer::reset_time() {
+    start_time = 0;
+    end_time = 0;
+}
+
+void Timer::display_time() {
+    std::cout << "Time elapsed: " << std::setprecision(3)
+              << std::fixed << get_time() << "ms, "
+              << get_time()/1000 << "s\n";
+}

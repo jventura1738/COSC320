8109fddbef39ac09e4b3e5054a2b9dc4d41579f1
Justin <justinv9001@aol.com>
2020-03-04 09:53:05 -0500

    update

--- Labs/Lab-5/main.cpp
+++ Labs/Lab-5/main.cpp
@@ -1,5 +1,6 @@
 #include "heapq.h"
 #include "jspace.h"
+#include "timer.h"
 #include <iostream>
 #include <algorithm>
 #include <cmath>
@@ -17,95 +18,116 @@ void random_swaps(T * arr, int length);
 
 int main() {
 
-    int qsize = 16;
-    HeapQ<int> pqueue(qsize);
+    Timer timer;
+    HeapQ<int> pq(1000000);
 
-    std::cout << "=== Priorities match values === \n";
-    std::cout << "Created a priority queue with 16 total spaces.\n";
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    srand(seed);
 
-    for (int i = 0; i < 16; i++) {
+    for (int i = 0; i < 1000000; i++) {
 
-    	std::cout << "enqueuing...\n";
-        pqueue.enqueue(i, i);
-        pqueue.print_queue(0, 5);
-
-    }
-
-    std::cout << "This will call push back.\n";
-    pqueue.enqueue(16, 16);
-    pqueue.print_queue(0, 5);
-
-    std::cout << "Adding value: 999 with priority 999.\n";
-    pqueue.enqueue(999, 999);
-    pqueue.print_queue(0, 5);
-
-    std::cout << "\nNow print the priority queue from highest to lowest priority:\n";
-    for (int i = 0; i < pqueue.length; i++) {
-
-        std::cout << pqueue.dequeue() << " ";
+        int val = (rand() % 9999) + 1;
+        int priority = (rand() % 999) + 1;
+        pq.enqueue(val, priority);
 
     }
-    std::cout << "\n";
 
-    std::cout << "\n=== STRING TEST ===\n";
-    std::cout << "Priority queue by age test. \n\n";
-    std::cout << "List of names with ages:\n";
+    std::cout << "Test time to enqueue 5 items which will rise to top of the queue.\n";
+    timer.start_timer();
+    pq.enqueue(0, 1001);
+    timer.end_timer();
+    timer.display_time();
 
-    std::string names[] = {"Justin Ventura", "Jacob Duncan", "Joe Anderson", "Ian Thomas",
-    "Gabriel Corinthian", "Michael Mandulak", "Blaine Mason", "Zak Holliman"};
-    int ages[] = {20, 19, 29, 20, 19, 18, 19, 21};
-    HeapQ<std::string> pq(qsize);
-    qsize = 8;
+
+    // int qsize = 16;
+    // HeapQ<int> pqueue(qsize);
+
+    // std::cout << "=== Priorities match values === \n";
+    // std::cout << "Created a priority queue with 16 total spaces.\n";
+
+    // for (int i = 0; i < 16; i++) {
+
+    // 	std::cout << "enqueuing...\n";
+    //     pqueue.enqueue(i, i);
+    //     pqueue.print_queue(0, 5);
+
+    // }
+
+    // std::cout << "This will call push back.\n";
+    // pqueue.enqueue(16, 16);
+    // pqueue.print_queue(0, 5);
+
+    // std::cout << "Adding value: 999 with priority 999.\n";
+    // pqueue.enqueue(999, 999);
+    // pqueue.print_queue(0, 5);
+
+    // std::cout << "\nNow print the priority queue from highest to lowest priority:\n";
+    // for (int i = 0; i < pqueue.length; i++) {
+
+    //     std::cout << pqueue.dequeue() << " ";
+
+    // }
+    // std::cout << "\n";
+
+    // std::cout << "\n=== STRING TEST ===\n";
+    // std::cout << "Priority queue by age test. \n\n";
+    // std::cout << "List of names with ages:\n";
+
+    // std::string names[] = {"Justin Ventura", "Jacob Duncan", "Joe Anderson", "Ian Thomas",
+    // "Gabriel Corinthian", "Michael Mandulak", "Blaine Mason", "Zak Holliman"};
+    // int ages[] = {20, 19, 29, 20, 19, 18, 19, 21};
+    // HeapQ<std::string> pq(qsize);
+    // qsize = 8;
     
-    for (int i = 0; i < qsize; i++) {
+    // for (int i = 0; i < qsize; i++) {
 
-        std::cout << names[i] << " age: " << ages[i] << "\n";
-        pq.enqueue(names[i], ages[i]);
+    //     std::cout << names[i] << " age: " << ages[i] << "\n";
+    //     pq.enqueue(names[i], ages[i]);
 
-    }
+    // }
 
-    std::cout << "\nTest peek (should print 'Joe Anderson'):\n";
-    std::cout << pq.peek() << "\n\n";
-    std::cout << "Now the lists will be re-ordered by age priority:\n";
+    // std::cout << "\nTest peek (should print 'Joe Anderson'):\n";
+    // std::cout << pq.peek() << "\n\n";
+    // std::cout << "Now the lists will be re-ordered by age priority:\n";
 
-    jspace::sort(ages, qsize);
-    jspace::reverse(ages, qsize);
+    // jspace::sort(ages, qsize);
+    // jspace::reverse(ages, qsize);
 
-    for (int i = 0; i < qsize; i++) {
+    // for (int i = 0; i < qsize; i++) {
 
-        names[i] = pq.dequeue();
+    //     names[i] = pq.dequeue();
 
-    }
+    // }
 
-    for (int i = 0; i < qsize; i++) {
+    // for (int i = 0; i < qsize; i++) {
 
-        std::cout << names[i] << " age: " << ages[i] << "\n";
+    //     std::cout << names[i] << " age: " << ages[i] << "\n";
 
-    }
+    // }
 
-    std::cout << "\n===== EXTRA CREDIT =====\n";
-    int n = 100;
-    int arr1[n];
-    int arr2[n];
-    for (int i = 0; i < n; i++) {
+    // std::cout << "\n===== EXTRA CREDIT =====\n";
+    // int n = 100;
+    // int arr1[n];
+    // int arr2[n];
+    // for (int i = 0; i < n; i++) {
 
-        arr1[i] = i;
-        arr2[i] = i;
+    //     arr1[i] = i;
+    //     arr2[i] = i;
 
-    }
-    std::cout << "Initial array:\n";
-    jspace::print(arr1, n);
+    // }
+    // std::cout << "Initial array:\n";
+    // jspace::print(arr1, n);
 
-    // randomized priority algorithm.
-    shuffle_arr(arr1, n);
-    // random swaps algorithm.
-    random_swaps(arr2, n);
+    // // randomized priority algorithm.
+    // shuffle_arr(arr1, n);
+    // // random swaps algorithm.
+    // random_swaps(arr2, n);
 
-    std::cout << "\nResulting array from random priorities:\n";
-    jspace::print(arr1, n);
+    // std::cout << "\nResulting array from random priorities:\n";
+    // jspace::print(arr1, n);
 
-    std::cout << "Resulting array from random swaps:\n";
-    jspace::print(arr2, n);
+    // std::cout << "Resulting array from random swaps:\n";
+    // jspace::print(arr2, n);
 
     return 0;
 }

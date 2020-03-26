26575c2d3f4365244517df0854183492f0514010
Justin Ventura <justinv9001@aol.com>
2020-03-26 10:31:55 -0400

    gamers

---
+++ SpellCheck/main.cpp
@@ -0,0 +1,16 @@
+// Justin Ventura COSC320-002
+// Project 2: Spell Check
+// File: main.cpp 03/26/20
+
+#include <iostream>
+#include <fstream>
+#include "jspace.h"
+
+// 
+void loadDatabase();
+
+int main() {
+
+
+	return 0;
+}

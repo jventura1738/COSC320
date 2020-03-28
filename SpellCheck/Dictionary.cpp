8a06c7b6383909cf4cc9ec5fe8e2506f759d5d18
Justin Ventura <justinv9001@aol.com>
2020-03-28 19:44:03 -0400

    stop looking at my code buddy

---
+++ SpellCheck/Dictionary.cpp
@@ -0,0 +1,5 @@
+// Justin Ventura COSC320-002
+// Project 2: Spell Check
+// File: Dictionary.cpp 03/26/20
+
+#include "Dictionary.h"
---
+++ SpellCheck/Dictionary.h
@@ -0,0 +1,18 @@
+// Justin Ventura COSC320-002
+// Project 2: Spell Check
+// File: Dictionary.h 03/26/20
+
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include <iostream>
+#include <cmath.h>
+#include "chain.h"
+
+class Dictionary {
+
+
+
+};
+
+#endif
